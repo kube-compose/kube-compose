27b17450459f3f73df8a148425423136ec6c86e0
jbrekelmans <jasperbrekelmans@gmail.com>
2019-04-08 09:25:48 +1000

    fix #15, #14 and #17

--- pkg/config/ports.go
+++ pkg/config/ports.go
@@ -3,12 +3,26 @@ package config
 import (
 	"fmt"
 	"strconv"
+	"strings"
+
+	"github.com/pkg/errors"
 )
 
 type Port struct {
 	ContainerPort int32
 	ExternalPort  int32
 	Protocol      string
+}
+
+func parsePortUint(portStr string) (int32, error) {
+	port, err := strconv.ParseUint(portStr, 10, 64)
+	if err != nil {
+		return -1, errors.Wrap(err, fmt.Sprintf("unsupported port format %s", portStr))
+	}
+	if port >= 65536 {
+		return -1, fmt.Errorf("port must be < 65536 but got %d", port)
+	}
+	return int32(port), nil
 }
 
 // https://docs.docker.com/compose/compose-file/compose-file-v2/
@@ -22,23 +36,43 @@ type Port struct {
 //  - "127.0.0.1:5000-5010:5000-5010"
 //  - "6060:6060/udp"
 //  - "12400-12500:1240"
-func parsePorts(inPorts []string) ([]Port, error) {
+func parsePorts(inPorts []port) ([]Port, error) {
 	n := len(inPorts)
 	if n == 0 {
 		return nil, nil
 	}
 	outPorts := make([]Port, n)
-	for i, portStr := range inPorts {
-		port, err := strconv.ParseUint(portStr, 10, 64)
-		if err != nil {
-			return nil, err
-		}
-		if port >= 65536 {
-			return nil, fmt.Errorf("port must be < 65536 but got %d", port)
+	for i, portRaw := range inPorts {
+		portRawStr := portRaw.Value
+		colonPos := strings.IndexByte(portRawStr, ':')
+		var containerPort int32
+		var externalPort int32
+		if colonPos >= 0 {
+			externalPortStr := portRawStr[:colonPos]
+			containerPortStr := portRawStr[colonPos+1:]
+			if strings.IndexByte(containerPortStr, ':') >= 0 {
+				return nil, fmt.Errorf("unsupported port format %s", portRawStr)
+			}
+			var err error
+			externalPort, err = parsePortUint(externalPortStr)
+			if err != nil {
+				return nil, err
+			}
+			containerPort, err = parsePortUint(containerPortStr)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			var err error
+			containerPort, err = parsePortUint(portRawStr)
+			if err != nil {
+				return nil, err
+			}
+			externalPort = containerPort
 		}
 		outPorts[i] = Port{
-			ContainerPort: int32(port),
-			ExternalPort:  int32(port),
+			ContainerPort: containerPort,
+			ExternalPort:  externalPort,
 			Protocol:      "TCP",
 		}
 	}
