acd42030469db753951175d69e961e8626b94f0d
Jasper Brekelmans <jasperbrekelmans@gmail.com>
2019-04-24 09:51:36 +0700

    fix jbrekelmans/kube-compose#31: support port ranges and full docker port bindings in config module

---
+++ pkg/config/util.go
@@ -0,0 +1,15 @@
+package config
+
+import "regexp"
+
+func buildRegexpMatchMap(r *regexp.Regexp, matches []string) map[string]string {
+	subexpNames := r.SubexpNames()
+	n := len(subexpNames)
+	matchMap := map[string]string{}
+	for i = 1; i < n; i++ {
+		if len(subexpNames[i]) > 0 {
+			matchMap[subxpNames[i]] = matches[i]
+		}
+	}
+	return matchMap
+}
