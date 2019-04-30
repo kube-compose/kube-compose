88f9aafa2f02338d0fdbb60c9f5ab8b4d2374be0
Russell Clarke <russell.clarke@anz.com>
2019-04-30 16:28:35 +1000

    add extends functionality for environment and image

--- pkg/config/merge.go
+++ pkg/config/merge.go
@@ -1,10 +1,16 @@
 package config
 
-// base is
+// extendedService is merged into service
 func merge(service *Service, extendedService *Service) {
-	mergeEnvironment(service.Environment, extendedService.Environment)
+	// rules based on https://docs.docker.com/compose/extends/#adding-and-overriding-configuration
+	mergeMap(service.Environment, extendedService.Environment)
+	if len(extendedService.Image) > 0 {
+		service.Image = extendedService.Image
+	}
 }
 
-func mergeEnvironment(env map[string]string, extendedEnv map[string]string) {
-
+func mergeMap(env map[string]string, extendedEnv map[string]string) {
+	for k, v := range extendedEnv {
+		env[k] = v
+	}
 }
