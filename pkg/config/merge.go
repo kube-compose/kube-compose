37261348ab7e1ab645cea2cc8c3cea8122596705
russ-c <4328524+russ-c@users.noreply.github.com>
2019-05-13 15:34:12 +1000

    fix pointer bug

--- pkg/config/merge.go
+++ pkg/config/merge.go
@@ -4,7 +4,7 @@ package config
 func merge(service, extendedService *Service) {
 	// rules based on https://docs.docker.com/compose/extends/#adding-and-overriding-configuration
 	mergeStringMap(service.Environment, extendedService.Environment)
-	mergePortBindings(service.Ports, extendedService.Ports)
+	mergePortBindings(&service.Ports, extendedService.Ports)
 	// TODO https://github.com/jbrekelmans/kube-compose/issues/48
 }
 
@@ -16,6 +16,6 @@ func mergeStringMap(intoStringMap, fromStringMap map[string]string) {
 	}
 }
 
-func mergePortBindings(intoPorts []PortBinding, fromPorts []PortBinding) {
-	intoPorts = append(intoPorts, fromPorts...)
+func mergePortBindings(intoPorts *[]PortBinding, fromPorts []PortBinding) {
+	*intoPorts = append(*intoPorts, fromPorts...)
 }
