47524403129eaf4dd32a64e1efc3f91e824ac6ea
russ-c <4328524+russ-c@users.noreply.github.com>
2019-05-13 13:26:32 +1000

    initial commit #53

--- pkg/config/merge.go
+++ pkg/config/merge.go
@@ -1,9 +1,14 @@
 package config
 
+import (
+	"fmt"
+)
+
 // extendedService is merged into service
 func merge(service, extendedService *Service) {
 	// rules based on https://docs.docker.com/compose/extends/#adding-and-overriding-configuration
 	mergeStringMap(service.Environment, extendedService.Environment)
+	mergePortBindings(service.Ports, extendedService.Ports)
 	// TODO https://github.com/jbrekelmans/kube-compose/issues/48
 }
 
@@ -13,4 +18,11 @@ func mergeStringMap(intoStringMap, fromStringMap map[string]string) {
 			intoStringMap[k] = v
 		}
 	}
+}
+
+func mergePortBindings(intoPorts []PortBinding, fromPorts []PortBinding) {
+	fmt.Println(intoPorts)
+	fmt.Println(fromPorts)
+	intoPorts = append(intoPorts, fromPorts...)
+	fmt.Println(intoPorts)
 }
