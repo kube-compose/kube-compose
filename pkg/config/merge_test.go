ba93d3bb9cab1a5afe74862299e4dc1d21c6d21b
russ-c <4328524+russ-c@users.noreply.github.com>
2019-05-15 13:38:24 +1000

    add unit tests #99

---
+++ pkg/config/merge_test.go
@@ -0,0 +1,80 @@
+package config
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMergePortBindings_Basic(t *testing.T) {
+	intoPorts := []PortBinding{{80, 80, 80, "tcp", ""}}
+	fromPorts := []PortBinding{{8000, 8000, 8000, "tcp", ""}}
+	expected := []PortBinding{{80, 80, 80, "tcp", ""}, {8000, 8000, 8000, "tcp", ""}}
+
+	intoPorts = mergePortBindings(intoPorts, fromPorts)
+	if !reflect.DeepEqual(intoPorts, expected) {
+		t.Fail()
+	}
+}
+
+func TestMergePortBindings_Duplicate(t *testing.T) {
+	intoPorts := []PortBinding{{80, 80, 80, "tcp", ""}, {8000, 8000, 8000, "tcp", ""}}
+	fromPorts := []PortBinding{{8000, 8000, 8000, "tcp", ""}}
+	expected := []PortBinding{{80, 80, 80, "tcp", ""}, {8000, 8000, 8000, "tcp", ""}}
+
+	intoPorts = mergePortBindings(intoPorts, fromPorts)
+	if !reflect.DeepEqual(intoPorts, expected) {
+		t.Fail()
+	}
+}
+
+func TestMergePortBindings_DuplicateInternalOnly(t *testing.T) {
+	intoPorts := []PortBinding{{80, 80, 80, "tcp", ""}, {8000, 8001, 8001, "tcp", ""}}
+	fromPorts := []PortBinding{{8000, 8000, 8000, "tcp", ""}}
+	expected := []PortBinding{{80, 80, 80, "tcp", ""}, {8000, 8001, 8001, "tcp", ""}, {8000, 8000, 8000, "tcp", ""}}
+
+	intoPorts = mergePortBindings(intoPorts, fromPorts)
+	if !reflect.DeepEqual(intoPorts, expected) {
+		t.Fail()
+	}
+}
+
+func TestMergePortBindings_Empty(t *testing.T) {
+	intoPorts := []PortBinding{}
+	fromPorts := []PortBinding{}
+	expected := []PortBinding{}
+
+	intoPorts = mergePortBindings(intoPorts, fromPorts)
+	if !reflect.DeepEqual(intoPorts, expected) {
+		t.Fail()
+	}
+}
+func TestMergeStringMaps_Basic(t *testing.T) {
+	intoStringMap := map[string]string{"a": "b"}
+	fromStringMap := map[string]string{"c": "d"}
+	expected := map[string]string{"a": "b", "c": "d"}
+
+	mergeStringMaps(intoStringMap, fromStringMap)
+	if !reflect.DeepEqual(intoStringMap, expected) {
+		t.Fail()
+	}
+}
+func TestMergeStringMaps_Duplicate(t *testing.T) {
+	intoStringMap := map[string]string{"a": "b", "c": "d"}
+	fromStringMap := map[string]string{"c": "d"}
+	expected := map[string]string{"a": "b", "c": "d"}
+
+	mergeStringMaps(intoStringMap, fromStringMap)
+	if !reflect.DeepEqual(intoStringMap, expected) {
+		t.Fail()
+	}
+}
+func TestMergeStringMaps_Empty(t *testing.T) {
+	intoStringMap := map[string]string{}
+	fromStringMap := map[string]string{}
+	expected := map[string]string{}
+
+	mergeStringMaps(intoStringMap, fromStringMap)
+	if !reflect.DeepEqual(intoStringMap, expected) {
+		t.Fail()
+	}
+}
