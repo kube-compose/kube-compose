cb12a2cf7d79e21f06aa0daec4cc242dada22f81
russ-c <4328524+russ-c@users.noreply.github.com>
2019-05-15 13:38:24 +1000

    add unit tests #99

--- pkg/config/merge.go
+++ pkg/config/merge.go
@@ -3,12 +3,12 @@ package config
 // extendedService is merged into service
 func merge(service, extendedService *Service) {
 	// rules based on https://docs.docker.com/compose/extends/#adding-and-overriding-configuration
-	mergeStringMap(service.Environment, extendedService.Environment)
+	mergeStringMaps(service.Environment, extendedService.Environment)
 	service.Ports = mergePortBindings(service.Ports, extendedService.Ports)
 	// TODO https://github.com/jbrekelmans/kube-compose/issues/48
 }
 
-func mergeStringMap(intoStringMap, fromStringMap map[string]string) {
+func mergeStringMaps(intoStringMap, fromStringMap map[string]string) {
 	for k, v := range fromStringMap {
 		if _, ok := intoStringMap[k]; !ok {
 			intoStringMap[k] = v
@@ -16,7 +16,7 @@ func mergeStringMap(intoStringMap, fromStringMap map[string]string) {
 	}
 }
 
-func mergePortBindings(intoPorts []PortBinding, fromPorts []PortBinding) []PortBinding {
+func mergePortBindings(intoPorts, fromPorts []PortBinding) []PortBinding {
 	for _, v := range fromPorts {
 		intoPorts = appendPortBindingIfUnique(intoPorts, v)
 	}
