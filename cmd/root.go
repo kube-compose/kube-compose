0d2c25be193de342dbd2d1003455adc86ea5d818
Michael <Michael.Fornaro@anz.com>
2019-05-16 16:21:38 +1000

    refactor port command to describe

--- cmd/root.go
+++ cmd/root.go
@@ -16,7 +16,7 @@ var rootCmd = &cobra.Command{
 }
 
 func Execute() {
-	rootCmd.AddCommand(SetupDownCli(), SetupUpCli(), SetupPortCli())
+	rootCmd.AddCommand(SetupDownCli(), SetupUpCli(), SetupDescribeCli())
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
