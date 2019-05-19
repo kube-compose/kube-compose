ba5e4b0fa52aac3c90e012566bc846eb62d88907
jbrekelmans <4394224+jbrekelmans@users.noreply.github.com>
2019-05-19 23:32:10 +1000

    issue #108: improve coverage in pkg/docker/compose/config/config.go

--- pkg/docker/compose/config/config_test.go
+++ pkg/docker/compose/config/config_test.go
@@ -1,41 +1,77 @@
 package config
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestConfigLoaderParseEnvironment_Success(t *testing.T) {
-
-}
-
-/*
-
-func (c *configLoader) parseEnvironment(env []environmentNameValuePair) (map[string]string, error) {
-	envParsed := make(map[string]string, len(env))
-	for _, pair := range env {
-		var value string
-		if pair.Name == "" {
-			return nil, fmt.Errorf("invalid environment variable: %s", pair.Name)
-		}
-		switch {
-		case pair.Value == nil:
-			var ok bool
-			if value, ok = c.environmentGetter(pair.Name); !ok {
-				continue
-			}
-		case pair.Value.StringValue != nil:
-			value = *pair.Value.StringValue
-		case pair.Value.Int64Value != nil:
-			value = strconv.FormatInt(*pair.Value.Int64Value, 10)
-		case pair.Value.FloatValue != nil:
-			value = strconv.FormatFloat(*pair.Value.FloatValue, 'g', -1, 64)
-		default:
-			// Environment variables with null values in the YAML are ignored.
-			// See test/docker-compose.null-env.yml.
-			continue
-		}
-		envParsed[pair.Name] = value
+	name1 := "CFGLOADER_PARSEENV_VAR1"
+	value1 := "CFGLOADER_PARSEENV_VAL1"
+	name2 := "CFGLOADER_PARSEENV_VAR2"
+	name3 := "CFGLOADER_PARSEENV_VAR3"
+	name4 := "CFGLOADER_PARSEENV_VAR4"
+	input := []environmentNameValuePair{
+		{
+			Name: name1,
+		},
+		{
+			Name: name2,
+			Value: &environmentValue{
+				StringValue: new(string),
+			},
+		},
+		{
+			Name: name3,
+			Value: &environmentValue{
+				Int64Value: new(int64),
+			},
+		},
+		{
+			Name: name4,
+			Value: &environmentValue{
+				FloatValue: new(float64),
+			},
+		},
+		{
+			Name:  "CFGLOADER_PARSEENV_VAR5",
+			Value: &environmentValue{},
+		},
+		{
+			Name: "CFGLOADER_PARSEENV_VAR6",
+		},
 	}
-	return envParsed, nil
+	m := map[string]string{
+		name1: value1,
+	}
+	c := &configLoader{
+		environmentGetter: mapValueGetter(m),
+	}
+	output, err := c.parseEnvironment(input)
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(output, map[string]string{
+		name1: value1,
+		name2: "",
+		name3: "0",
+		name4: "0",
+	}) {
+		t.Error(output)
+	}
 }
-*/
+func TestConfigLoaderParseEnvironment_InvalidName(t *testing.T) {
+	input := []environmentNameValuePair{
+		{
+			Name: "",
+		},
+	}
+	m := map[string]string{}
+	c := &configLoader{
+		environmentGetter: mapValueGetter(m),
+	}
+	_, err := c.parseEnvironment(input)
+	if err == nil {
+		t.Fail()
+	}
+}
