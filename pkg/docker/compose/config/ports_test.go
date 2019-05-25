7918e994fca91e8021ebe2e46d8f3d5aaf025811
Jasper Brekelmans <4394224+jbrekelmans@users.noreply.github.com>
2019-05-25 16:12:14 +1000

    Feature/#120 (#146)
    * issue #120: fix docker compose ports binding unit tests, fix bugb
    
    * issue #120: fix linting

--- pkg/docker/compose/config/ports_test.go
+++ pkg/docker/compose/config/ports_test.go
@@ -1,80 +1,136 @@
 package config
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestParsePortBindings_InternalMinTooLarge(t *testing.T) {
-	portBindings := []PortBinding{}
-	_, err := parsePortBindings("65536", portBindings)
+	_, err := parsePortBindings("65536", nil)
 	if err == nil {
 		t.Fail()
 	}
 }
 
 func TestParsePortBindings_InternalMaxTooLarge(t *testing.T) {
-	portBindings := []PortBinding{}
-	_, err := parsePortBindings("65535-65536", portBindings)
+	_, err := parsePortBindings("65535-65536", nil)
 	if err == nil {
 		t.Fail()
 	}
 }
 
 func TestParsePortBindings_ExternalMinTooLarge(t *testing.T) {
-	portBindings := []PortBinding{}
-	_, err := parsePortBindings("65536:8000", portBindings)
+	_, err := parsePortBindings("65536:8000", nil)
 	if err == nil {
 		t.Fail()
 	}
 }
 
 func TestParsePortBindings_ExternalMaxTooLarge(t *testing.T) {
-	portBindings := []PortBinding{}
-	_, err := parsePortBindings("65535-65536:8000-8001", portBindings)
+	_, err := parsePortBindings("65535-65536:8000-8001", nil)
 	if err == nil {
 		t.Fail()
 	}
 }
 
 func TestParsePortBindings_RandomlyAvailable(t *testing.T) {
-	portBindings := []PortBinding{}
-	_, err := parsePortBindings("8000-8001:8000", portBindings)
+	expected := []PortBinding{
+		{
+			Internal:    8000,
+			ExternalMin: 8000,
+			ExternalMax: 8001,
+			Protocol:    "tcp",
+		},
+	}
+	actual, err := parsePortBindings("8000-8001:8000", nil)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
+	} else if !reflect.DeepEqual(actual, expected) {
+		t.Logf("ports1: %+v\n", actual)
+		t.Logf("ports2: %+v\n", expected)
+		t.Fail()
 	}
 }
 
 func TestParsePortBindings_RangeLengthMismatch(t *testing.T) {
-	portBindings := []PortBinding{}
-	_, err := parsePortBindings("8000-8002:8000-8001", portBindings)
+	_, err := parsePortBindings("8000-8002:8000-8001", nil)
 	if err == nil {
 		t.Fail()
 	}
 }
 
 func TestParsePortBindings_SuccessWithExternal1(t *testing.T) {
-	portBindings := []PortBinding{}
-	_, err := parsePortBindings("127.0.0.1:8000-8001:8000-8001/udp", portBindings)
+	expected := []PortBinding{
+		{
+			Internal:    8000,
+			ExternalMin: 8000,
+			ExternalMax: 8000,
+			Protocol:    "udp",
+			Host:        "127.0.0.1",
+		},
+		{
+			Internal:    8001,
+			ExternalMin: 8001,
+			ExternalMax: 8001,
+			Protocol:    "udp",
+			Host:        "127.0.0.1",
+		},
+	}
+	actual, err := parsePortBindings("127.0.0.1:8000-8001:8000-8001/udp", nil)
 	if err != nil {
 		t.Fatal(err)
+	} else if !reflect.DeepEqual(actual, expected) {
+		t.Logf("ports1: %+v\n", actual)
+		t.Logf("ports2: %+v\n", expected)
+		t.Fail()
 	}
 }
 func TestParsePortBindings_SuccessWithExternal2(t *testing.T) {
-	portBindings := []PortBinding{}
-	_, err := parsePortBindings("127.0.0.1:8000:8000/udp", portBindings)
+	expected := []PortBinding{
+		{
+			Internal:    8000,
+			ExternalMin: 8000,
+			ExternalMax: 8000,
+			Protocol:    "udp",
+			Host:        "127.0.0.1",
+		},
+	}
+	actual, err := parsePortBindings("127.0.0.1:8000:8000/udp", nil)
 	if err != nil {
 		t.Fatal(err)
+	} else if !reflect.DeepEqual(actual, expected) {
+		t.Logf("ports1: %+v\n", actual)
+		t.Logf("ports2: %+v\n", expected)
+		t.Fail()
 	}
 }
 
 func TestParsePortBindings_SuccessWithoutExternal(t *testing.T) {
-	portBindings := []PortBinding{}
-	_, err := parsePortBindings("8000-8001", portBindings)
+	expected := []PortBinding{
+		{
+			Internal:    8000,
+			ExternalMin: -1,
+			ExternalMax: -1,
+			Protocol:    "tcp",
+		},
+		{
+			Internal:    8001,
+			ExternalMin: -1,
+			ExternalMax: -1,
+			Protocol:    "tcp",
+		},
+	}
+	actual, err := parsePortBindings("8000-8001", nil)
 	if err != nil {
 		t.Fatal(err)
+	} else if !reflect.DeepEqual(actual, expected) {
+		t.Logf("ports1: %+v\n", actual)
+		t.Logf("ports2: %+v\n", expected)
+		t.Fail()
 	}
 }
 func TestParsePortBindings_Error(t *testing.T) {
-	portBindings := []PortBinding{}
-	_, err := parsePortBindings("!", portBindings)
+	_, err := parsePortBindings("!", nil)
 	if err == nil {
 		t.Fail()
 	}
