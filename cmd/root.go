29e3caeb1199027427ff9bd9321cc99e6a597e00
jbrekelmans <4394224+jbrekelmans@users.noreply.github.com>
2019-06-03 07:30:00 +1000

    issue #177: improve docs/README.md

--- cmd/root.go
+++ cmd/root.go
@@ -26,7 +26,8 @@ func Execute() error {
 
 func setRootCommandFlags(rootCmd *cobra.Command) {
 	rootCmd.PersistentFlags().StringP(fileFlagName, "f", "", "Specify an alternate compose file")
-	rootCmd.PersistentFlags().StringP(namespaceFlagName, "n", "", "namespace for environment")
+	rootCmd.PersistentFlags().StringP(namespaceFlagName, "n", "", fmt.Sprintf("namespace for environment. Can also be set via " +
+		"environment variable %sNAMESPACE", envVarPrefix))
 	rootCmd.PersistentFlags().StringP(envIDFlagName, "e", "", "used to isolate environments deployed to a shared namespace, "+
 		"by (1) using this value as a suffix of pod and service names and (2) using this value to isolate selectors. Either this flag or "+
 		fmt.Sprintf("the environment variable %sENVID must be set", envVarPrefix))
