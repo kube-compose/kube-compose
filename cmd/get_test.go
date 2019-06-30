a5268e771c1364e0ab82e70cf77d83d6659cc54b
jbrekelmans <4394224+jbrekelmans@users.noreply.github.com>
2019-07-01 07:52:09 +1000

    Revert "fixes #225: remove bad test"
    This reverts commit b18a2c49b469843e5db85463ccef8e0db429b6f0.

--- cmd/get_test.go
+++ cmd/get_test.go
@@ -14,3 +14,12 @@ func TestGetCommand_NoArgsError(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestGetCommand_ConfigError(t *testing.T) {
+	cmd := &cobra.Command{}
+	args := []string{"authentication-service"}
+	err := getCommand(cmd, args)
+	if err == nil {
+		t.Fail()
+	}
+}
