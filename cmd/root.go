ffeda9e8ef1d1ab4e5270acb266f03dbde909e03
jbrekelmans <4394224+jbrekelmans@users.noreply.github.com>
2019-06-30 11:30:29 +1000

    fixes #217: fix test coverage

--- cmd/root.go
+++ cmd/root.go
@@ -3,58 +3,19 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"strings"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
 )
 
 const (
 	envVarPrefix        = "KUBECOMPOSE_"
 	fileFlagName        = "file"
-	logLevelEnvVarName  = envVarPrefix + "LOGLEVEL"
-	logLevelFlagName    = "log-level"
 	namespaceEnvVarName = envVarPrefix + "NAMESPACE"
 	namespaceFlagName   = "namespace"
 	envIDEnvVarName     = envVarPrefix + "ENVID"
 	envIDFlagName       = "env-id"
 )
-
-var formattedLogLevelList = formatLogLevelList()
-
-func formatLogLevelList() string {
-	var sb strings.Builder
-	sb.WriteString(log.AllLevels[0].String())
-	iLast := len(log.AllLevels) - 1
-	for i := 1; i < iLast; i++ {
-		sb.WriteString(", ")
-		sb.WriteString(log.AllLevels[i].String())
-	}
-	sb.WriteString(" and ")
-	sb.WriteString(log.AllLevels[iLast].String())
-	return sb.String()
-}
-
-func getLogLevelFlag(flags *pflag.FlagSet) (log.Level, error) {
-	if !flags.Changed(logLevelFlagName) {
-		s, exists := envGetter(logLevelEnvVarName)
-		if !exists {
-			return log.WarnLevel, nil
-		}
-		logLevel, err := log.ParseLevel(s)
-		if err != nil {
-			return 0, fmt.Errorf("the environment variable %s can only be set to one of %s", logLevelEnvVarName, formattedLogLevelList)
-		}
-		return logLevel, nil
-	}
-	s, _ := flags.GetString(logLevelFlagName)
-	logLevel, err := log.ParseLevel(s)
-	if err != nil {
-		return 0, fmt.Errorf("the flag --%s can only be set to one of %s", logLevelFlagName, formattedLogLevelList)
-	}
-	return logLevel, nil
-}
 
 func Execute() error {
 	log.SetOutput(os.Stdout)
