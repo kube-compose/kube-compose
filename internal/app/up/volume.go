e69bcf753fa24db762b44e2cbc78969f4c22387f
Jurgen Braam <2862977+jubr@users.noreply.github.com>
2023-10-17 19:00:04 +0200

    buildVolumeInitImage: output JSON in case of error

--- internal/app/up/volume.go
+++ internal/app/up/volume.go
@@ -282,7 +282,11 @@ func buildVolumeInitImage(
 		return nil, err
 	}
 	r := &buildVolumeInitImageResult{}
-	decoder := json.NewDecoder(response.Body)
+
+	// duplicate the Reader, so we can print the json content on error
+	var bodyContent bytes.Buffer
+	jsonTee := io.TeeReader(response.Body, &bodyContent)
+	decoder := json.NewDecoder(jsonTee)
 	for {
 		var msg jsonmessage.JSONMessage
 		err = decoder.Decode(&msg)
@@ -298,8 +302,8 @@ func buildVolumeInitImage(
 		}
 	}
 	if r.imageID == "" {
-		log.Warnf("Json content: %s\n", response.Body)
-		return nil, fmt.Errorf("could not parse image ID from docker build output stream")
+		log.Warnf("ImageBuild() JSON response: %s\n", bodyContent)
+		return nil, fmt.Errorf("buildVolumeInitImage: could not parse image ID from docker build output stream")
 	}
 	return r, nil
 }
